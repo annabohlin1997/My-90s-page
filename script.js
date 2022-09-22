3728f9d76cb48284fbc9aa26d55473cd6e9d4ced
annaBohlin <anna@bohlinoffice.se>
2022-09-22 21:06:14 +0200

    Made background fixed

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function changeText() {
     let myElement = document.createElement('h2');
     myElement.innerHTML = 'The answer is 3! Did you get it right?';
 
-    document.getElementById('guess').appendChild(myElement);
+    document.getElementById('oscars-guess').appendChild(myElement);
 }
 
-let button = document.getElementById('button');
+let button = document.getElementById('answer-button');
 button.addEventListener('click', changeText);
 
 new cursoreffects.fairyDustCursor({colors: ["#d8bfd8", "#005d82dd", "#ffffff"]});
